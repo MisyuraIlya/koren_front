9afe362be7a650502f415210da3a3aaea661bf2d
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2024-04-03 16:22:52 +0300

    add

---
+++ src/app/(teacher)/teacher/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react';
+import Auth from '@/modules/auth';
+import { Box } from '@mui/material';
+
+const page = () => {
+    return (
+      <Box sx={{width:'60%'}}>
+            teacher page
+      </Box>
+    );
+};
+
+export default page;
