65d8f37ad1ddb45231edaf732ca946e3af00eacb
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2024-04-03 16:11:04 +0300

    fix provider

--- src/app/(student)/layout.tsx
+++ src/app/(student)/layout.tsx
@@ -1,14 +1,13 @@
 "use client"
 import { StudentCoursesProvider } from "@/modules/student/provider/StudentCoursesProvider";
-import CoursesPageComponent from "@/modules/student/components/courses/CoursesPageComponent";
-
+import { CoursesProvider } from "@/provider/CourseProvider";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <StudentCoursesProvider>
-        <CoursesPageComponent>
+        <CoursesProvider>
           {children}
-        </CoursesPageComponent>
+        </CoursesProvider>
       </StudentCoursesProvider>
     </>
   );
