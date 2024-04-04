2f87e11eb2660617efbe5b3711bd7d6fb2bcbad7
ilya.mi@digitrade.co.il <ilya.mi@digitrade.co.il>
2024-04-04 09:25:24 +0300

    add config

--- next.config.js
+++ next.config.js
@@ -1,53 +1,124 @@
 /** @type {import('next').NextConfig} */
 
-const LOCAL_API_BASE_PATH = 'http://localhost:4001'
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const {headers} = require("next/headers");
+
+const path = require('path')
+
+const LOCAL_API_BASE_PATH = 'http://localhost'
 const PUBLIC_API_BASE_PATH = 'http://3.74.228.194:4001'
 
-const nextConfig = {
+const PUBLIC_API_MEDIA = 'http://localhost:3000'
+const BACKEND_ENDPOINT_PORT = '4001'
 
-  reactStrictMode: false,
-  env: {
-    SERVER_URL: process.env.SERVER_URL
-  }, 
-  images: {
-    domains: ['localhost','3.74.228.194']
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/auth/:slug*',
-        destination: `http://3.74.228.194:4001/auth/:slug*`
-      },
-      {
-        source: '/courses/:slug*',
-        destination: `http://3.74.228.194:4001/courses/:slug*`
-      },
-      {
-        source: '/exercises/:slug*',
-        destination: `http://3.74.228.194:4001/exercises/:slug*`
-      },
-      {
-        source: '/answers/:slug*',
-        destination: `http://3.74.228.194:4001/answers/:slug*`
-      },
-      {
-        source: '/school/:slug*',
-        destination: `http://3.74.228.194:4001/answers/:slug*`
-      },
-      {
-        source: '/class/:slug*',
-        destination: `http://3.74.228.194:4001/answers/:slug*`
-      },
-      {
-        source: '/group/:slug*',
-        destination: `http://3.74.228.194:4001/answers/:slug*`
-      },
-      {
-        source: '/confirmation/:slug*',
-        destination: `http://3.74.228.194:4001/answers/:slug*`
-      }
-    ]
+
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    output: 'standalone',
+    reactStrictMode:false,
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
+    images: {
+      domains: ['localhost']
+    },
   }
-}
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...nextConfig,
+      //reactStrictMode: true,
+      env: {
+        ...nextConfig.env,
+        NEXT_PUBLIC_APP_ENTRYPOINT: `${LOCAL_API_BASE_PATH}`,
+        NEXT_PUBLIC_MEDIA: PUBLIC_API_MEDIA,
+      },
+      async rewrites() {
+        return [
+          {
+            source: '/auth/:slug*',
+            destination: `http://localhost:${BACKEND_ENDPOINT_PORT}/auth/:slug*`
+          },
+          {
+            source: '/courses/:slug*',
+            destination: `http://localhost:${BACKEND_ENDPOINT_PORT}/courses/:slug*`
+          },
+          {
+            source: '/exercises/:slug*',
+            destination: `http://localhost:${BACKEND_ENDPOINT_PORT}/exercises/:slug*`
+          },
+          {
+            source: '/answers/:slug*',
+            destination: `http://localhost:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+          },
+          {
+            source: '/school/:slug*',
+            destination: `http://localhost:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+          },
+          {
+            source: '/class/:slug*',
+            destination: `http://localhost:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+          },
+          {
+            source: '/group/:slug*',
+            destination: `http://localhost:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+          },
+          {
+            source: '/confirmation/:slug*',
+            destination: `http://localhost:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+          }
+        ]
+      }
+    }
+  }
 
-module.exports = nextConfig
+  return {
+    ...nextConfig,
+    env: {
+      ...nextConfig.env,
+      NEXT_PUBLIC_APP_ENTRYPOINT: `${PUBLIC_API_BASE_PATH}/api`,
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/auth/:slug*',
+          destination: `http://${PUBLIC_API_BASE_PATH}:${BACKEND_ENDPOINT_PORT}/auth/:slug*`
+        },
+        {
+          source: '/courses/:slug*',
+          destination: `http://${PUBLIC_API_BASE_PATH}:${BACKEND_ENDPOINT_PORT}/courses/:slug*`
+        },
+        {
+          source: '/exercises/:slug*',
+          destination: `http://${PUBLIC_API_BASE_PATH}:${BACKEND_ENDPOINT_PORT}/exercises/:slug*`
+        },
+        {
+          source: '/answers/:slug*',
+          destination: `http://${PUBLIC_API_BASE_PATH}:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+        },
+        {
+          source: '/school/:slug*',
+          destination: `http://${PUBLIC_API_BASE_PATH}:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+        },
+        {
+          source: '/class/:slug*',
+          destination: `http://${PUBLIC_API_BASE_PATH}:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+        },
+        {
+          source: '/group/:slug*',
+          destination: `http://${PUBLIC_API_BASE_PATH}:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+        },
+        {
+          source: '/confirmation/:slug*',
+          destination: `http://${PUBLIC_API_BASE_PATH}:${BACKEND_ENDPOINT_PORT}/answers/:slug*`
+        }
+      ]
+    }
+  }
+
+}
